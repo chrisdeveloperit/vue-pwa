0d1f01bf294611cec04bf3f7902ceaa5dddd2cb2
Chris <chrisdeveloper@gmail.com>
2021-06-02 08:10:34 -0700

    Create About page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,21 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <AboutUs msg="ABOUT ALL OF US" />
   </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import AboutUs from "@/components/AboutUs.vue"; 
+
+@Component({
+  components: {
+    AboutUs,
+  },
+})
+export default class About extends Vue {}
+</script>
+
+<style scoped>
+
+</style>
